f574e6c2f072c78bcb740c5a6e2d206d277b768c
Akashi <akashi_sai@163.com>
2022-07-30 17:53:44 +0800

    :tada: chore: update deps

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import { defineAsyncComponent } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
+const TextEditor = defineAsyncComponent(() => import('@use-kit/editor-vue'))
+console.log('t: ', TextEditor)
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <HelloWorld msg="Vue 3 + TypeScript + Vite" />
+  <div id="editor">
+    <TextEditor />
+  </div>
 </template>
 
 <style>
