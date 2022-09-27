07127c1626311d42fe36eeb7259f010b3685dc45
akashi <akashi_sai@163.com>
2022-09-27 18:06:30 +0800

    :tada: feat: encrypt

--- src/components/SetupView.vue
+++ src/components/SetupView.vue
@@ -1,5 +1,8 @@
 <script lang="ts">
 import { defineComponent, reactive, ref, toRef } from 'vue'
+import md5 from 'crypto-js/md5'
+import Base64 from 'crypto-js/enc-base64'
+import aes from 'aes-everywhere'
 
 export default defineComponent({
   async setup(props, ctx) {
@@ -11,8 +14,19 @@ export default defineComponent({
     })
     setTimeout(() => user.value = { id: '02', name: 'asuka' }, 1000)
 
+    const pwd = Base64.stringify(md5('123456'))
+    console.log('pwd', pwd)
+
+    const aesPwd = aes.encrypt('123456', 'PASSWORD')
+    console.log('aes pwd', aesPwd)
+
+    const decode = aes.decrypt(aesPwd, 'PASSWORD')
+    console.log('decrypt', decode)
+
     return {
       user,
+      pwd,
+      aesPwd,
     }
   }
 })
@@ -21,4 +35,6 @@ export default defineComponent({
 <template>
   <div>setup</div>
   <span>user: {{ user }}</span>
+  <div>password: {{ pwd }}</div>
+  <div>aes password: {{ aesPwd }}</div>
 </template>
