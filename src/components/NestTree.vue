f2030776170c34eddb28cb8994f81ef271d226ba
akashi <=>
2022-10-15 10:18:11 +0800

    :tada: feat: nest tree

---
+++ src/components/NestTree.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+defineProps<{ nodeData: any }>()
+</script>
+
+<template>
+  <ul>
+    <li
+      v-for="node of nodeData"
+      :key="node.key"
+      :value="node.value"
+      :title="node.title"
+    >
+      {{ node.title }}
+      <template v-if="node.children">
+        <NestTree :node-data="node.children" />
+      </template>
+    </li>
+  </ul>
+</template>
