4abbffbd8a398350bd535f772b44da40ffbe59bd
Akashi <akashi_sai@163.com>
2022-11-08 20:58:36 +0800

    :tada: feat: @composable && nest-tree

---
+++ packages/composable/src/components/index.ts
@@ -0,0 +1,1 @@
+export { default as NestTree } from './nestTree/NestTree.vue'
---
+++ packages/composable/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
+import libCss from 'vite-plugin-libcss'
+
+export default defineConfig({
+  build: {
+    minify: false,
+    lib: {
+      entry: 'src/index.ts',
+      name: '@composable',
+      fileName: 'composable'
+    },
+  },
+  plugins: [
+    vue(),
+    dts({
+      outputDir: 'dist/types'
+    }),
+    libCss(),
+  ]
+})
