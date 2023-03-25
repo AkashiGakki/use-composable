43b5131d122900f0badbb81e3cb31c0a5ba21cde
Akashi <akashi_sai@163.com>
2023-03-25 18:14:13 +0800

    :tada: feat: use

---
+++ playground/src/components/use/UseNow.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { useNow } from '@vueuse/core'
+import { useDate } from '../../hooks'
+
+const now = useDate(useNow() as any)
+</script>
+
+<template>
+  <div class="use-now">now: {{ now }}</div>
+</template>
---
+++ playground/src/components/use/UseTheme.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark({
+  selector: 'html',
+  attribute: 'color-scheme',
+  valueDark: 'dark',
+  valueLight: 'light',
+})
+const toggleDark = useToggle(isDark)
+</script>
+
+<template>
+  <div class="use-theme">
+    is dark: {{ isDark }}
+    <button @click="toggleDark()">
+      toggle dark
+    </button>
+  </div>
+</template>
