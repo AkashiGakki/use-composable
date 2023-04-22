671076cf2398d3eb0eafa1033518e212860c797a
Akashi <akashi_sai@163.com>
2023-04-22 16:38:30 +0800

    :pencil: docs: vitepress support

---
+++ packages/docs/.vitepress/config.ts
@@ -0,0 +1,76 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Use Composables",
+
+  appearance: 'dark',
+
+  description: "A collection of utility functions based on Composition API",
+
+  markdown: {
+    theme: {
+      dark: 'dracula-soft',
+      light: 'vitesse-light',
+    },
+
+    attrs: {
+      leftDelimiter: '%{',
+      rightDelimiter: '}%',
+    },
+  },
+
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    logo: '/favicon.png',
+
+    nav: [
+      { text: 'Guide', link: '/guide/' },
+      { text: 'API', link: '/api/', activeMatch: '^/api/' },
+      { text: 'Examples', link: '/markdown-examples' },
+      { text: 'Home', link: '/' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/akashigakki/use-composables' }
+    ],
+
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2022-PRESENT Akashi Sai'
+    },
+
+    // 搜索
+    // algolia: {
+    //   appId: '8J64VVRP8K',
+    //   apiKey: 'a18e2f4cc5665f6602c5631fd868adfd',
+    //   indexName: 'vitepress'
+    // },
+  },
+
+  head: [
+    ['meta', { name: 'theme-color', content: '#ffffff' }],
+    ['link', { rel: 'icon', href: '/favicon-32x32.png', type: 'image/png' }],
+    ['link', { rel: 'icon', href: '/favicon.png', type: 'image/svg+xml' }],
+    ['meta', { name: 'author', content: 'Akashi Sai' }],
+
+    ['meta', { property: 'og:title', content: 'UseComposables' }],
+    ['meta', { property: 'og:image', content: 'https://vueuse.org/og.png' }],
+    ['meta', { property: 'og:description', content: 'Collection of essential Vue Composition Utilities' }],
+
+    ['link', { rel: 'dns-prefetch', href: 'https://fonts.gstatic.com' }],
+    ['link', { rel: 'preconnect', crossorigin: 'anonymous', href: 'https://fonts.gstatic.com' }],
+    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap' }],
+    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Fira+Code&display=swap' }],
+  ],
+})
---
+++ packages/docs/index.md
@@ -0,0 +1,28 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Use Composables"
+  text: Collection of Vue Composition Utilities
+  tagline: "A Collection of Utility Functions Based on Vue Composition API"
+  image:
+    src: /favicon.png
+    alt: UseComposable
+  actions:
+    - theme: brand
+      text: Markdown Examples
+      link: /markdown-examples
+    - theme: alt
+      text: API Examples
+      link: /api-examples
+
+features:
+  - title: 💡 Intuitive
+    details: The API has been designed to provide a well-sample user experience.
+  - title: 🔑 Type Strong
+    details: Flexible programmatic APIs with full TypeScript typing.
+  - title: 🚀 Seamless migration
+    details: Works seamlessly with both Vue 3 and 2
+---
+
