c3000758afc96b4bd47b10c72fdb2e8a6d648662
akashi <akashi_sai@163.com>
2023-04-24 09:07:26 +0800

    :rocket: chore: intrgrated deploy

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     lib: {
       entry: 'src/index.ts',
       name: '@use-composables/components',
-      fileName: 'use-composables/components',
+      fileName: 'index',
     },
   },
   plugins: [
