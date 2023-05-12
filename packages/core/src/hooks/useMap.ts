b7f022e09da3c4805714f82605b19c8c56e53d5c
Akashi <akashi_sai@163.com>
2023-05-12 20:07:40 +0800

    :pencil: docs: complete document

--- packages/core/src/hooks/useMap.ts
+++ packages/core/src/hooks/useMap.ts
@@ -4,15 +4,15 @@ import { isMap } from '@use-kit/functions'
 export const useMap = (uniqueKey = 'key') => {
   const map = reactive(new Map())
 
-  const set = (field: Record<string, unknown>) => map.set(field[uniqueKey], field)
+  const set = (field: Record<string | number, unknown>) => map.set(field[uniqueKey], field)
 
-  const setAll = (fields: Record<string, unknown>[]) => fields.forEach(f => set(f))
+  const setAll = (fields: Record<string | number, unknown>[]) => fields.forEach(f => set(f))
 
-  const get = (key: string) => map.get(key)
+  const get = (key: string | number) => map.get(key)
 
   const reset = () => map.clear()
 
-  const remove = (key: string) => map.delete(key)
+  const remove = (key: string | number) => map.delete(key)
 
   const getMap = (m?: Map<unknown, unknown>) => {
     if (isMap(m)) {
@@ -27,7 +27,7 @@ export const useMap = (uniqueKey = 'key') => {
 
   const values = () => Array.from(map.values())
 
-  const has = (key: string) => map.has(key)
+  const has = (key: string | number) => map.has(key)
 
   const toArray = () => values()
 
