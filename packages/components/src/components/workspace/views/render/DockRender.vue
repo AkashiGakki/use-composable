41d117efe601380be62a143cd0b92a674660d161
akashi <akashi_sai@163.com>
2023-07-25 15:33:14 +0800

    :zap: chore: default render

--- packages/components/src/components/workspace/views/render/DockRender.vue
+++ packages/components/src/components/workspace/views/render/DockRender.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import { computed, inject, reactive } from 'vue-demi'
+import { computed, inject, onMounted, reactive, ref } from 'vue-demi'
 import { isArray } from '@use-kit/functions'
 import type { IDockConfig, Workspace } from '@use-composable/definition'
 
 import DockRenderer from '../dock/DockRenderer.vue'
 import DockTabRenderer from '../dock/DockTabRenderer.vue'
+import DefaultRender from './DefaultRender.vue'
 
 const props = defineProps<{
   params: any
@@ -14,15 +15,27 @@ const $workspace = inject<Workspace>('$workspace')
 
 const dock: IDockConfig = reactive(props.params.dock ?? {})
 const isRenderTabs = computed(() => dock?.tabs && isArray(dock?.tabs))
+const isDefaultRender = ref(true)
+
+onMounted(() => {
+  // setTimeout to translate default to actual render
+  setTimeout(() => {
+    isDefaultRender.value = false
+  })
+})
 </script>
 
 <template>
   <div class="default-dock-render">
-    <!-- tags load service -->
-    <DockTabRenderer v-if="isRenderTabs" :dock="dock" />
+    <DefaultRender v-if="isDefaultRender" />
 
-    <!-- root load service -->
-    <DockRenderer v-else :dock="dock" />
+    <div v-else>
+      <!-- tags load service -->
+      <DockTabRenderer v-if="isRenderTabs" :dock="dock" />
+
+      <!-- root load service -->
+      <DockRenderer v-else :dock="dock" />
+    </div>
   </div>
 </template>
 
