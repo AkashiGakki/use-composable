3f7568bbc0417c62273f6ff7501fe8157720cbba
akashi <akashi_sai@163.com>
2023-12-14 15:31:24 +0800

    :tada: feat: media-render

---
+++ packages/components/src/components/im/config/index.ts
@@ -0,0 +1,19 @@
+import audioIcon from '@ui/im/assets/audio.svg'
+import excelIcon from '@ui/im/assets/excel.svg'
+import pdfIcon from '@ui/im/assets/pdf.svg'
+import pptIcon from '@ui/im/assets/ppt.svg'
+import textIcon from '@ui/im/assets/text.svg'
+import videoIcon from '@ui/im/assets/video.svg'
+import wordIcon from '@ui/im/assets/word.svg'
+import zipIcon from '@ui/im/assets/zip.svg'
+
+export {
+  audioIcon,
+  excelIcon,
+  pdfIcon,
+  pptIcon,
+  textIcon,
+  videoIcon,
+  wordIcon,
+  zipIcon,
+}
